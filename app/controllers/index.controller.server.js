a6f94917e1b528c0384b1b64cd81d4d8cc9c871f
AminaAidarus <aaidaru1@my.centennialcollege.ca>
2022-09-23 20:37:05 -0400

    modified:   app/controllers/index.controller.server.js 	modified:   app/routes/index.route.server.js 	new file:   app/views/content/contact.ejs 	new file:   app/views/content/projects.ejs 	new file:   app/views/content/services.ejs 	modified:   app/views/index.ejs

--- app/controllers/index.controller.server.js
+++ app/controllers/index.controller.server.js
@@ -1,5 +1,21 @@
 //Index controller
 
-export function helloPlain(req, res, next){
-    res.render('index',{title: 'Home'});
+export function displayHomePage(req, res, next){
+    res.render('index',{title: 'Home', page: 'home'});
+}
+
+export function displayAboutPage(req, res, next){
+    res.render('index',{title: 'About', page: 'about' });
+}
+
+export function displayContactsPage(req, res, next){
+    res.render('index',{title: 'Contacts', page: 'contacts' });
+}
+
+export function displayProjectsPage(req, res, next){
+    res.render('index',{title: 'Projects', page: 'projects' });
+}
+
+export function displayServicesPage(req, res, next){
+    res.render('index',{title: 'Services', page: 'services' });
 }
--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,11 +1,18 @@
 // Index Routes 
 import { Router } from 'express';
-import { helloPlain } from '../controllers/index.controller.server.js';
-import { helloHtml } from '../controllers/index.controller.server.js';
+import { displayHomePage } from '../controllers/index.controller.server.js';
+import { displayAboutPage } from '../controllers/index.controller.server.js';
+import { displayContactsPage } from '../controllers/index.controller.server.js';
+import { displayProjectsPage } from '../controllers/index.controller.server.js';
+import { displayServicesPage } from '../controllers/index.controller.server.js';
 const router = Router();
 
 // add middleware to connect application
-router.get('/',helloPlain);
-router.get('/html',helloHtml);
+router.get('/home',displayHomePage);
+router.get('/services',displayServicesPage);
+router.get('/projects',displayProjectsPage);
+router.get('/about',displayAboutPage);
+router.get('/contacts',displayContactsPage);
+
 
 export default router;
